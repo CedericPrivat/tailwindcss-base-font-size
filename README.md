e9e5294b740bdd785275de030297a8d1a58415ae
Cederic Privat <cederic.privat@gmail.com>
2023-06-24 13:30:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# tailwindcss-base-font-size
+
+A Tailwind CSS plugin to set the base font size in proportion to Tailwind's default utility values.
+
+## Why Use This Plugin?
+
+The purpose of this plugin is to allow you to easily set the base font size to `10px`, which is equivalent to `1rem`. By doing so, all rem values become easier to read and work with. Setting a base font size of `10px` simplifies calculations with rem values, making it more convenient to use arbitrary values or adding custom theme values.
+
+By using this plugin, you can customize the base font size and ensure that all utility classes in Tailwind CSS retain their relative proportions.
+
+## Installation
+
+Install the plugin from npm:
+
+```sh
+npm install @tailwindcss/container-queries
+```
+
+Then add the plugin to your `tailwind.config.js` file:
+
+```js
+// tailwind.config.js
+module.exports = {
+  theme: {
+    // ...
+  },
+  plugins: [
+    require('tailwindcss-base-font-size'),
+    // ...
+  ],
+};
+```
+
+## Usage
+
+Customize the base font size by passing an optional baseFontSize parameter to the plugin:
+
+```js
+// tailwind.config.js
+module.exports = {
+  theme: {
+    // ...
+  },
+  plugins: [
+    require('tailwindcss-base-font-size')({
+      baseFontSize: 12,
+    }),
+    // ...
+  ],
+};
+```
+
+The default value is `10`.
+
+## Tailwind CSS IntelliSense Extension
+
+If you are using the Tailwind CSS IntelliSense extension in Visual Studio Code, it's recommended to update your `settings.json` file to ensure that the preview correctly reflects the base font size. Add the following configuration to your `settings.json`:
+
+```json
+{
+  // ...
+  "tailwindCSS.rootFontSize": 10
+}
+```
+
+This configuration sets the root font size to `10px`, matching the base font size specified by this plugin. With this setting, the IntelliSense extension will provide accurate and consistent previews for Tailwind CSS utility classes.
+
+## How It Works
+
+The plugin modifies the base font size of your HTML elements and adjusts the values of Tailwind CSS utility classes to maintain their relative sizes.
+
+It adds a CSS rule for the `html` element, setting its font size to the specified `baseFontSize` value.
+
+```css
+html: {
+  font-size: 10px;
+}
+```
+
+Additionally, it extends the Tailwind CSS theme by updating the rem values in the theme configuration scaled proportionally with respect to the given `baseFontSize`. This ensures that utility classes like `text-lg`, `p-4`, etc., continue to have the same relative sizes.
---
+++ src/index.ts
@@ -0,0 +1,97 @@
+import { Config } from 'tailwindcss';
+import defaultConfig from 'tailwindcss/defaultConfig';
+import plugin from 'tailwindcss/plugin';
+import resolveConfig from 'tailwindcss/resolveConfig';
+
+interface Options {
+  baseFontSize?: number;
+}
+
+type ThemeConfig = Config['theme'];
+type ResolvedConfig = ReturnType<typeof resolveConfig>;
+
+const DEFAULT_BASE_FONT_SIZE = 10;
+
+export const baseFontSizePlugin = plugin.withOptions<Options>(
+  ({ baseFontSize = DEFAULT_BASE_FONT_SIZE } = {}) => {
+    return ({ addBase }) => {
+      addBase({
+        html: {
+          fontSize: `${baseFontSize}px`,
+        },
+      });
+    };
+  },
+  ({ baseFontSize = DEFAULT_BASE_FONT_SIZE } = {}) => {
+    const config = resolveConfig(defaultConfig);
+
+    const updatedTheme = getUpdatedTheme(config, baseFontSize);
+
+    return {
+      theme: {
+        extend: { ...updatedTheme },
+      },
+    };
+  }
+);
+
+function getUpdatedTheme(config: ResolvedConfig, baseFontSize: number): ThemeConfig {
+  if (!config.theme) return;
+
+  const updatedTheme: ThemeConfig = {};
+
+  for (const [key, value] of Object.entries(config.theme)) {
+    if (typeof value === 'object' && value !== null) {
+      const updatedProperty = getUpdatedRemValues(value, baseFontSize);
+
+      if (hasRemValue(updatedProperty)) {
+        updatedTheme[key] = updatedProperty;
+      }
+    }
+  }
+
+  return updatedTheme;
+}
+
+function getUpdatedRemValues(value: any, baseFontSize: number): any {
+  if (typeof value === 'string') {
+    if (value.endsWith('rem')) {
+      const numericValue = parseFloat(value);
+      return `${(numericValue * 16) / baseFontSize}rem`;
+    } else {
+      return value;
+    }
+  } else if (Array.isArray(value)) {
+    const updatedArray: any[] = [];
+    for (const item of value) {
+      const updatedItem = getUpdatedRemValues(item, baseFontSize);
+      if (updatedItem !== null) {
+        updatedArray.push(updatedItem);
+      }
+    }
+    return updatedArray.length > 0 ? updatedArray : null;
+  } else if (typeof value === 'object' && value !== null) {
+    const updatedObject: { [key: string]: any } = {};
+    for (const [key, nestedValue] of Object.entries(value)) {
+      const updatedValue = getUpdatedRemValues(nestedValue, baseFontSize);
+      if (updatedValue !== null) {
+        updatedObject[key] = updatedValue;
+      }
+    }
+    return Object.keys(updatedObject).length > 0 ? updatedObject : null;
+  } else {
+    return null;
+  }
+}
+
+function hasRemValue(obj: any): boolean {
+  if (typeof obj === 'string') {
+    return obj.endsWith('rem');
+  } else if (Array.isArray(obj)) {
+    return obj.some(hasRemValue);
+  } else if (typeof obj === 'object' && obj !== null) {
+    return Object.values(obj).some(hasRemValue);
+  } else {
+    return false;
+  }
+}
